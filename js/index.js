3cae85228dc0ae870d7bf23fd1601067624fbe83
Casper Pijnenburg <casperpijnenburg@gmail.com>
2018-12-18 14:29:57 +0100

    choice based on userid

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+const urls = ["/html/vigor.html", "/html/clarity.html", "/html/reason.html", "/html/resolve.html"]
+
 document.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch('http://localhost:3000/config');
     const config = await response.json();
-    
+
     const service = new FacebookService(config);
     service.init();
 
@@ -10,9 +12,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         const accessToken = response.credential.accessToken;
         const userId = response.additionalUserInfo.profile.id;
 
-        const urls = ["/html/vigor.html", "/html/clarity.html", "/html/reason.html", "/html/resolve.html"]
-
-        const choice = urls[Math.floor(Math.random() * urls.length)];
+        const index = userId % urls.length;
+        const choice = urls[index];
 
         window.location += choice;
     })
