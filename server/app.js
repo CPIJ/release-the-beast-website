5334c98ef9f61ee8d576a43e062416c40b984d53
Casper Pijnenburg <casperpijnenburg@gmail.com>
2018-12-18 12:09:31 +0100

    Added env variables

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+app.get('/config', (req, res) => {
+    res.send({
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
+    });
+});
+
+app.listen(3000)
